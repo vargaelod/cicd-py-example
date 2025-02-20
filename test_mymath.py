0739ade82132c840aa626406d1302c0c2743f664
madorjan2 <76928045+madorjan2@users.noreply.github.com>
2025-02-20 19:15:33 +0100

    Update test_mymath.py

--- test_mymath.py
+++ test_mymath.py
@@ -1,7 +1,6 @@
 import pytest
 
 from mymath import add_numbers
-from mymath import mult_numbers
 
 def test_add_positive():
     assert add_numbers(1, 2) == 3
@@ -16,8 +15,3 @@ def test_add_string__expect_exception():
     with pytest.raises(TypeError):
         add_numbers(4, 'I DO NOT BELONG HERE')
 
-def test_mult_positive():
-    assert mult_numbers(1, 2) == 2
-
-def test_mult_zero():
-    assert mult_numbers(1, 0) == 0
